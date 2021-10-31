dccafcb63839d9c1762443db400056eaf47cf08d
Jeremy Albright <1935258+Js-Brecht@users.noreply.github.com>
2021-10-31 10:46:48 -0800

    one more subdirectory

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-const content = require("../content/test.raw?-treeshake&keepUnused");
+const content = require("../../content/test.raw?-treeshake&keepUnused");
 
 export default function handler(req, res) {
   res.status(200).json({ content })
